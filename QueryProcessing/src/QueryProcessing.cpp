8a92c57afb1432f4c4df1b7c060a126b45783f9c
Tejal Sabnis <tejal.sabnis@gmail.com>
2017-03-16 01:19:03 -0700

    skeleton code

---
+++ QueryProcessing/src/QueryProcessing.cpp
@@ -0,0 +1,24 @@
+/*
+ * QueryProcessing.cpp
+ *
+ *  Created on: Mar 16, 2017
+ *      Author: tejal
+ */
+
+class QueryProcessing {
+
+	/*GeometryCollection processQuery (QueryTree qt) {
+
+	}
+
+	std::vector materializeLeft () {
+
+	}
+
+	std::vector materializeRight () {
+
+	}*/
+
+};
+
+
---
+++ QueryProcessing/src/QueryTree.cpp
@@ -0,0 +1,12 @@
+/*
+ * QueryTree.cpp
+ *
+ *  Created on: Mar 16, 2017
+ *      Author: tejal
+ */
+
+
+class QueryTree {
+
+};
+
