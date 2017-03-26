13dac7f09a75b1c60a36f637ff947f7bac8a2af4
vnikhil <nvementa@asu.edu>
2017-03-26 09:50:42 -0700

    a) Commit test
    b) Added query_result

---
+++ QueryProcessing/src/headerfiles/query_result.h
@@ -0,0 +1,17 @@
+/*
+ * query_result.h
+ *
+ *  Created on: Mar 26, 2017
+ *      Author: vnikhil
+ */
+
+#include "geometry.h"
+
+#ifndef HEADERFILES_QUERY_RESULT_H_
+#define HEADERFILES_QUERY_RESULT_H_
+
+class QueryResult {
+
+};
+
+#endif /* HEADERFILES_QUERY_RESULT_H_ */
