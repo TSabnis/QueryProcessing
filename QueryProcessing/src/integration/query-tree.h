eb4d93022a5d5090f5ccba07708b997c7a889e3c
Tejal Sabnis <tejal.sabnis@gmail.com>
2017-04-01 10:59:02 -0700

    update from integration repo

--- QueryProcessing/src/integration/query-tree.h
+++ QueryProcessing/src/integration/query-tree.h
@@ -1,13 +1,25 @@
-#include <string>
-#include "geometry.h"
+#include "spatial-index-interface.h"
 
 class QueryTree {
+    vector<string> root;
+    vector<string> leftFilter;
+    PointCollection leftDataPoint;
+    RectangleCollection leftDataRect;
+    vector<string> rightFilter;
+    PointCollection rightDataPoint;
+    RectangleCollection rightDataRect;
+    SpatialIndexInterface rightIndexedObject;
+	SpatialIndexInterface leftIndexedObject;
+
 public:
-	vector<string> root;
-	vector<string> leftFilter;
-	PointCollection leftDataPoint;
-	RectangleCollection leftDataRect;
-	vector<string> rightFilter;
-	PointCollection rightDataPoint;
-	RectangleCollection rightDataRect;
+    QueryTree();
+    void setRoot(vector<string>);
+    void setLeftFilter(vector<string>);
+    void setLeftPoints(PointCollection);
+    void setLeftRectangles(RectangleCollection);
+    void setLeftIndexedObject(SpatialIndexInterface);
+    void setRightFilter(vector<string>);
+    void setRightPoints(PointCollection);
+    void setRightRectangles(RectangleCollection);
+    void setRightIndexedObject(SpatialIndexInterface);
 };
