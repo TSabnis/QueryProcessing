1d25b01bf609813159b3e729f4aef363d680b420
TejalSabnis <tejal.sabnis@gmail.com>
2017-04-01 17:42:28 -0700

    getArea in RectangleOperations and support for rectangles in distance for filterBy and area for filterBy

--- QueryProcessing/src/query-processing/RectangleOperations.cpp
+++ QueryProcessing/src/query-processing/RectangleOperations.cpp
@@ -42,6 +42,10 @@ public:
     static double getDistance (Rectangle rec1, Rectangle rec2){
         return boost::geometry::distance(box(point(rec1.getCoordinates().at(0),rec1.getCoordinates().at(1)), point(rec1.getCoordinates().at(2),rec1.getCoordinates().at(3))), box(point(rec2.getCoordinates().at(0),rec2.getCoordinates().at(1)), point(rec2.getCoordinates().at(2),rec2.getCoordinates().at(3))));
     }
+
+    static double getArea (Rectangle rec1){
+		return boost::geometry::area(box(point(rec1.getCoordinates().at(0),rec1.getCoordinates().at(1)), point(rec1.getCoordinates().at(2),rec1.getCoordinates().at(3))));
+	}
    
 
 };
